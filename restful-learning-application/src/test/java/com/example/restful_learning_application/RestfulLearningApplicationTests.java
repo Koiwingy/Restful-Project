89d4650e0b7eff90f4d527de88d5a4f66eb644d2
Koiwingy <kaixingsze@gmail.com>
2025-01-08 14:16:26 -0500

    Add files via upload

---
+++ restful-learning-application/src/test/java/com/example/restful_learning_application/RestfulLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restful_learning_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
