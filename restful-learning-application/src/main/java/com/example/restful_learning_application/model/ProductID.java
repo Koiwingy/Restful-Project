580cf32e4ae50c0b9b635b028f34d4b4d4d9ade4
Koiwingy <kaixingsze@gmail.com>
2025-01-08 15:39:14 -0500

    Add files via upload

---
+++ restful-learning-application/src/main/java/com/example/restful_learning_application/model/ProductID.java
@@ -0,0 +1,13 @@
+package com.example.restful_learning_application.model;
+
+public class ProductID {
+
+    private final String id;
+
+    public ProductID(final String id) {
+        this.id = id;
+    }
+        public String getID () {
+            return this.id;
+    }
+}
